a9d2ef1e67fa859055bc79e39bd30ebc5c85a660
Keith Perfetti <perfetti@hbnetworks.net>
2023-12-04 17:16:24 -0500

    Got the prompt working

--- 2023/day_3/solver.ts
+++ 2023/day_3/solver.ts
@@ -16,6 +16,9 @@ type PartNumber = GridDenizen & {
 const nonPeriodRegexMatcher = /(\d+)|([^\d.]+)/ig
 // Sample puzzle
 const engineSchematic = "\n467..114..\n...*......\n..35..633.\n......#...\n617*......\n.....+.58.\n..592.....\n......755.\n...$.*....\n.664.598.."
+const fs = require('fs');
+// const engineSchematic = fs.readFileSync('input.txt', 'utf8') as string;
+
 // We want to view this as a 2D array of characters, then find any numbers that are not touching any symbols besides a . even diagnally
 const schematicLines = engineSchematic.split("\n").filter(line => line.length > 0)
 
@@ -49,15 +52,14 @@ schematicLines.forEach((line, index) => {
 
   matches.forEach((match) => {
     const parsed = Number(match[0]);
-    console.log("match:", match[0], " ", match.index, " ", match.length
-    )
+    // console.log("match:", match[0], " ", match.index, " ", match.length)
     if(isNaN(parsed)) {
       const whammy = addWhammy({
         _raw: match[0],
         x: match.index as number,
         y
       })
-      console.log("y:", y, " ", whammy);
+      // console.log("y:", y, " ", whammy);
     } else {
       const partNumber = addPartNumber({
         _raw: match[0],
@@ -67,13 +69,31 @@ schematicLines.forEach((line, index) => {
         length: match[0].length
       })
     }
-    console.log("y:", y, " ", parsed);
+    // console.log("y:", y, " ", parsed);
   })
 });
 
 // Filter out partNumbers that are touching a whammy
-const validPartNumbers = [];
+const validPartNumbers: PartNumber[] = [];
 
+partNumbers.forEach((partNumber) => {
+  const {x, y, length} = partNumber;
+  let isValid = false;
+  // Check the offsets
+  [-1, 0, 1].forEach((yOffset) => {
+    // Iterate over whammies if they exist
+    whammiesByRow[y + yOffset]?.forEach((whammy) => {
+      // If it is touching a whammy even diagonally, it is valid
+      if(whammy.x >= x-1 && whammy.x <= x + length + 1) {
+        isValid = true;
+      }
+    })
+  });
 
-console.log('partNumbers:', partNumbersByRow)
+  if(isValid) {
+    validPartNumbers.push(partNumber)
+  }
+})
 
+console.log('Valid partNumbers:', validPartNumbers)
+console.log('Added up the partNumbers:', validPartNumbers.reduce((sum, partNumber) => sum + partNumber.partNumber, 0))
