35b66711a7975cafd80c03a4d4dec1ee7bba6022
Keith Perfetti <perfetti@hbnetworks.net>
2025-11-11 17:38:39 -0500

    finished day1

--- 2024/day1/step1.ts
+++ 2024/day1/step1.ts
@@ -4,20 +4,29 @@ const input = fs.readFileSync("./input.txt", "utf8");
 const lines = input.split("\n");
 const leftList: number[] = [];
 const rightList: number[] = [];
+const rightFrequencySet: Record<number, number> = {};
 
+// PrepData
 // Parse the input into two lists.
 lines.forEach((line) => {
-  const [left, right] = line.split(/\s+/);
-  leftList.push(parseInt(left));
-  rightList.push(parseInt(right));
+  const [left, right] = line.split(/\s+/).map((num) => parseInt(num));
+  leftList.push(left);
+  rightList.push(right);
+  rightFrequencySet[right] = (rightFrequencySet[right] || 0) + 1;
 });
 
 leftList.sort()
 rightList.sort()
 
+
+// Calculate things
 let totalDistance = 0;
+let similarityScore = 0;
+
 for (let i = 0; i < leftList.length; i++) {
   totalDistance += Math.abs(leftList[i] - rightList[i]);
+  similarityScore += leftList[i] * rightFrequencySet[leftList[i]] || 0;
 }
 
 console.log("totalDistance", totalDistance);
+console.log("similarityScore", similarityScore);
