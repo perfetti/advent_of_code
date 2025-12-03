039407d5272433efed2b04fb6e775ce2f165e6ee
Keith Perfetti <perfetti@hbnetworks.net>
2025-12-02 19:09:29 -0500

    2024 day2 in progress

--- 2024/day2/parse.ts
+++ 2024/day2/parse.ts
@@ -4,52 +4,110 @@ const sampleInput = fs.readFileSync("./sample.txt", "utf8");
 const sampleInputLines = sampleInput.split("\n");
 const input = fs.readFileSync("./input.txt", "utf8");
 const inputLines = input.split("\n");
-type Report = { levels: number[], safe: boolean, errorReason: string };
+interface iReport = { levels: number[], safe: boolean, badTransitions: number, errorReason: string[] };
 
+class Node {
+  public index: number;
+  public level: number;
 
-
-function parseSingleReport(line: string): Report {
-  let isSafe = true;
-    let errorReason = "";
-    const levelStrings = line.split(/\s+/)
-
-    const levels = levelStrings.map((curLevelString, index) => {
-    const currentLevel = parseInt(curLevelString);
-    const penUltimateLevel = parseInt(levelStrings[index - 2]);
-    const lastLevel = parseInt(levelStrings[index - 1]);
-
-      try {
-        if(lastLevel) {
-          const distance = Math.abs(lastLevel - currentLevel);
-          const wasRising = penUltimateLevel < lastLevel;
-          const isRising = lastLevel < currentLevel;
-
-          // console.log(penUltimateLevel, lastLevel, currentLevel, 'wasRising', wasRising, 'isRising', isRising);
-
-          if(distance == 0) throw new Error("Report is not safe, levels changing TOO LITTLE");
-          if(distance > 3) throw new Error("Report is not safe, levels changing TOO MUCH");
-          if(penUltimateLevel && wasRising !== isRising) throw new Error("Report is not safe, levels are not consistently rising or falling");
-        }
-      } catch (error) {
-        isSafe = false;
-        errorReason = error.message;
-      }
-
-      return currentLevel
-    });
-
-  return { levels, safe: isSafe, errorReason };
+  constructor(index: number, level: number) {
+    this.index = index;
+    this.level = level;
+  }
 }
 
+class Transition {
+  public wasRising: boolean;
+  public isRising: boolean;
+  public distance: number;
+  public errors: string[] = [];
+
+  constructor(public curNode: Node, public penUltimateNode: Node, public lastNode: Node) {
+    this.wasRising = penUltimateNode.level < lastNode.level;
+    this.isRising = lastNode.level < curNode.level;
+    this.distance = Math.abs(lastNode.level - curNode.level);
+  }
+
+  isSafe(): boolean {
+    if(Number.isNaN(this.lastNode.level)) return true;
+    if(this.distance == 0) this.errors.push("Report is not safe, levels changing TOO LITTLE");
+    if(this.distance > 3) this.errors.push("Report is not safe, levels changing TOO MUCH");
+    if(this.penUltimateNode.level && this.wasRising !== this.isRising) this.errors.push("Report is not safe, levels are not consistently rising or falling");
+    return this.errors.length === 0;
+  }
+}
+
+class Report {
+  public nodes: Node[] = [];
+  public transitions: Transition[] = [];
+  public isSafe: boolean = true;
+  public badTransitions: number = 0;
+  public errorReason: string[] = [];
+
+  constructor(public levels: number[]) {
+    this.nodes = levels.map((level, index) => new Node(index, level));
+  }
+
+  parse() {
+    this.levels.forEach((level, index) => {
+      const node = new Node(index, level);
+      const penUltimateNode = new Node(index - 2, this.levels[index - 2]);
+      const lastNode = new Node(index - 1, this.levels[index - 1]);
+    });
+
+    this.transitions.forEach((transition) => {
+      if(!transition.isSafe()) {
+        this.errorReason.push(...transition.errors);
+      }
+    });
+  }
+}
+
+function parseSingleReport(line: string): iReport {
+  let errorReason: string[] = [];
+  const levelStrings = line.split(/\s+/)
+  const levels: number[] = [];
+  const transitions: Transition[] = [];
+  const isSafe = true;
+
+  levelStrings.forEach((curLevelString, index) => {
+    const node = new Node(index, parseInt(curLevelString));
+    const penUltimateNode = new Node(index - 2, parseInt(levelStrings[index - 2]));
+    const lastNode = new Node(index - 1, parseInt(levelStrings[index - 1]));
+
+    const transition = new Transition(node, penUltimateNode, lastNode);
+
+    if(!transition.isSafe()) {
+      errorReason.push(...transition.errors);
+      // Now we see if skipping this node would make the report safe.
+      const nextIndex = index + 1;
+      const nextLevel = parseInt(levelStrings[nextIndex]);
+      const nextNode = new Node(nextIndex, nextLevel);
+      const nextTransition = new Transition(nextNode, lastNode, node);
+      if(nextTransition.isSafe()) {
+        isSafe = true;
+        badTransitions.push(transition);
+      }
+    }
+
+    levels.push(currentLevel);
+    transitions.push(transition);
+  });
+
+  const badTransitions = transitions.filter((transition) => !transition.isSafe());
+  // We allow 1 error per report.
+  return { levels, safe: isSafe, badTransitions: badTransitions.length, errorReason };
+}
+
+
 function parseFullReports(lines: string[]): [Report[], number] {
-  const reports: Report[] = [];
   let safeReportCount = 0;
 
-  lines.forEach((line) => {
-    const { levels, safe, errorReason } = parseSingleReport(line);
+  const reports: iReport[] = lines.map((line) => {
+    const report = parseSingleReport(line);
 
-    if(safe) safeReportCount++;
-    reports.push({ levels, safe, errorReason });
+    if(report.safe) safeReportCount++;
+    return report;
   });
 
   console.log(reports.slice(0, 5));
@@ -58,5 +116,5 @@ function parseFullReports(lines: string[]): [Report[], number] {
 }
 // console.log(parseSingleReport("1 3 6 7 9"));
 
-// parseFullReports(sampleInputLines);
-parseFullReports(inputLines);
+parseFullReports(sampleInputLines);
+// parseFullReports(inputLines);
