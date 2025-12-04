aad00dd452e683fd7c6fdaf11477891a14d641a6
Keith Perfetti <perfetti@hbnetworks.net>
2025-12-04 10:27:12 -0500

    This should work but doesnt yay advent of code. Next brute forcing

--- 2025/day03/voltage_shenanigans.ts
+++ 2025/day03/voltage_shenanigans.ts
@@ -1,7 +1,7 @@
 import fs from 'fs';
 
 // Lets' the debugging work.
-const INPUT_FILE = __dirname + "/demo_input.txt"
+const INPUT_FILE = __dirname + "/input.txt"
 
 // This brute force does not work for 12 digits because we'd do 16! permutations per row
 // We'll retry the old way for part 2
@@ -37,7 +37,7 @@ type IndexedValue = {
   index: number;
 }
 
-function find12DigitBatteryVoltage(battery: string) {
+function find12DigitBatteryVoltage(battery: string, log=false) {
   console.log("Working on ", battery)
   let digits: IndexedValue[] = battery.split('').map((value, index) => ({value: Number(value), index}) as IndexedValue)
 
@@ -45,11 +45,23 @@ function find12DigitBatteryVoltage(battery: string) {
   const firstDigits = digits.slice(0, battery.length - 12);
   const earliestDigit = findHighestEarlierValue(firstDigits);
 
-  console.log('earliestDigit', earliestDigit);
+  const matchingValuesInFirstDigits = firstDigits.filter(d => d.value === earliestDigit.value);
+
+  if(matchingValuesInFirstDigits.length > 1) {
+    console.log('multiple matching values in first digits', matchingValuesInFirstDigits);
+  }
+
+  const voltages = matchingValuesInFirstDigits.map(d => generateVoltageFromEarliestDigit(d, [...digits.map((d) => ({value: d.value, index: d.index}))], log));
+  console.log(voltages);
+  return Math.max(...voltages);
+}
+
+function generateVoltageFromEarliestDigit(earliestDigit: IndexedValue, digits: IndexedValue[], log:boolean=false) {
+  log && console.log('earliestDigit', earliestDigit);
   // printDigits(digits);
   for(let i=0; i<earliestDigit.index; i++){
     const removed = digits.shift();
-    console.log('removing from front', removed);
+    log && console.log('removing from front', removed);
   }
 
 
@@ -58,15 +70,17 @@ function find12DigitBatteryVoltage(battery: string) {
   // console.log('digits', digits);
   while(digits.length > 12) {
     const removed = digits.pop()
-    console.log('removing', removed);
+    log && console.log('removing', removed);
   }
 
   digits = digits.sort((a,b) => a.index - b.index);
   const digitsWithoutTheChaff = Number(digits.map(d => d.value).join(""));
-  console.log('digitsWithoutTheChaff', digitsWithoutTheChaff);
+  log && console.log('digitsWithoutTheChaff', digitsWithoutTheChaff);
 
   return digitsWithoutTheChaff;
 }
+
+
 
 function printDigits(digits: IndexedValue[]) {
   console.log(digits.map(d => d.value).join(""));
@@ -92,3 +106,12 @@ batteries.forEach((battery: string, battery_index: number) => {
 });
 
 console.log("Total voltage count:", voltageCount);
+console.log("Total voltage count part two:", voltageCountPartTwo);
+
+
+// ["987654321111111",
+// "811111111111119",
+// "234234234234278",
+// "818181911112111"].forEach((val) => {
+//   console.log(val, find12DigitBatteryVoltage(val))
+// })
